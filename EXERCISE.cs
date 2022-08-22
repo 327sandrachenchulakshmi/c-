f65b97c81d464012103224bfd7031d29501ce097
327sandrachenchulakshmi <107600861+327sandrachenchulakshmi@users.noreply.github.com>
2022-08-22 19:25:37 +0530

    Add files via upload

---
+++ EXERCISE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EXERCISE
+    {
+        public static void Main()
+        {
+            int x;
+            int y;
+            int z;
+            Console.WriteLine("enter first number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            z = Convert.ToInt32(Console.ReadLine());
+            int multiply = x * y * z;
+            Console.WriteLine($"the output is {multiply}");
+
+        }
+    }
+}
